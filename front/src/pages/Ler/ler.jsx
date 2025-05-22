9aa97b0208557a757d57941dd22e8f2b3f9c713b
Defala <defalaemprego@gmail.com>
2025-05-21 22:04:57 -0400

    to melado de porra quente

--- front/src/pages/Ler/ler.jsx
+++ front/src/pages/Ler/ler.jsx
@@ -1,5 +1,6 @@
- import './ler.css';
-import Header from '../../components/header' // Ajuste o caminho conforme sua estrutura
+import './ler.css';
+import Header from '../../components/header';
+import lerImage from '../../assets/ler.png';
 
 function Ler() {
   return (
@@ -12,14 +13,14 @@ function Ler() {
             <h2>Izabelle Dumont</h2>
             <p className="post-date">21 de Fev.</p>
             <p className="post-description">
-              Mulheres correm passando por barricadas de rua em chamas no distrito de Solino, em Porto Príncipe (Haiti), enquanto moradores pedem ajuda ao governo e protestam contra a falta de segurança da cidade. A mais recente tentativa externa de ajudar o governo interino a retomar o controle – uma “missão multinacional de apoio à segurança” liderada por tropas quenianas – está subfinanciada e passando por dificuldades após uma onda de massacres de gangues que matou mais de 350 pessoas.
+              Mulheres correm passando por barricadas de rua em chamas no distrito de Solino, em Porto Príncipe (Haiti), enquanto moradores pedem ajuda ao governo e protestam contra a falta de segurança da cidade. A mais recente tentativa externa de ajudar o governo interino a retomar o controle — uma “missão multinacional de apoio à segurança” liderada por tropas quenianas — está subfinanciada e passando por dificuldades após uma onda de massacres de gangues que matou mais de 350 pessoas.
             </p>
+          </div>
+          <div className="post-image">
+            <img src={lerImage} alt="Cena de protesto no Haiti" />
             <div className="debate-button-container">
               <button className="debate-button">Debater</button>
             </div>
-          </div>
-          <div className="post-image">
-            <img src="/eyes.png" alt="Cena de protesto no Haiti" />
           </div>
         </div>
       </main>
@@ -28,4 +29,3 @@ function Ler() {
 }
 
 export default Ler;
- 
