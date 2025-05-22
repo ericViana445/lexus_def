27de45a0745a51cf73443847c727101c0f780770
Defala <defalaemprego@gmail.com>
2025-05-22 06:23:16 -0400

    agora tá bom

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import IndexPage from './pages/index/index.jsx';
-import Home from './pages/HomeAll/home.jsx';
+import HomeProfessor from './pages/HomeAll/home.jsx';
 import OpcaoCadastro from './pages/OpcaoCadastro/index_cad.jsx';
 import CadastroAluno from './pages/CadastroAluno/index_cad_aluno.jsx';
 import CadastroProfessor from './pages/CadastroProfessor/index_cad_prof.jsx';
 import Chat from './pages/Chat/chat.jsx';
 import Login from './pages/Login/login.jsx';
 import Tema from './pages/Tema/tema.jsx'
+import HomeAluno from './pages/HomeAluno/homeAluno.jsx';
 
 const App = () => {
   console.log("✅ App carregado");
@@ -17,13 +18,14 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<IndexPage />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<HomeProfessor />} />
         <Route path="/cadastro" element={<OpcaoCadastro />} />
         <Route path="/cadastro-aluno" element={<CadastroAluno />} />
         <Route path="/cadastro-professor" element={<CadastroProfessor />} />
         <Route path="/chat" element={<Chat />} />
         <Route path="/login" element={<Login />} />
         <Route path="/tema" element={<Tema />} />
+        <Route path="/homeAluno" element={<HomeAluno />} />
       </Routes>
     </BrowserRouter>
   );
