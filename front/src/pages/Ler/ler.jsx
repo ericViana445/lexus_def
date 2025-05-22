8aa9207b5ffd54984b33f4a492e11928cd567ca3
Defala <defalaemprego@gmail.com>
2025-05-21 20:23:09 -0400

    pinto que ler duro

---
+++ front/src/pages/Ler/ler.jsx
@@ -0,0 +1,31 @@
+ import './ler.css';
+import Header from '../../components/header' // Ajuste o caminho conforme sua estrutura
+
+function Ler() {
+  return (
+    <div className="app">
+      <Header />
+
+      <main className="main">
+        <div className="post-card">
+          <div className="post-text">
+            <h2>Izabelle Dumont</h2>
+            <p className="post-date">21 de Fev.</p>
+            <p className="post-description">
+              Mulheres correm passando por barricadas de rua em chamas no distrito de Solino, em Porto Príncipe (Haiti), enquanto moradores pedem ajuda ao governo e protestam contra a falta de segurança da cidade. A mais recente tentativa externa de ajudar o governo interino a retomar o controle – uma “missão multinacional de apoio à segurança” liderada por tropas quenianas – está subfinanciada e passando por dificuldades após uma onda de massacres de gangues que matou mais de 350 pessoas.
+            </p>
+            <div className="debate-button-container">
+              <button className="debate-button">Debater</button>
+            </div>
+          </div>
+          <div className="post-image">
+            <img src="/eyes.png" alt="Cena de protesto no Haiti" />
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Ler;
+ 
